c625455e98014070cf2e18a71f7430903e6dca6f
wmf34a <wmf34a@naver.com>
2021-07-24 23:18:49 +0900

    btn add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-//import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h2>Lulu-gram</h2>
-      <p>test1</p>
-      <p>test1</p>
+      <Button variant="primary">Login</Button>{' '}
+      <Button variant="danger">Logout</Button>{' '}
     </div>
   );
 }
